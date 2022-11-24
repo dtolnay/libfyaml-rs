f0c87ce5bea315ad47d4c412cd1d00af0c4b5f8c
David Tolnay <dtolnay@gmail.com>
2022-11-24 14:57:25 -0800

    Rename anonymous union fy_event__bindgen_ty_1 to fy_event_data

--- libfyaml-sys/src/lib.rs
+++ libfyaml-sys/src/lib.rs
@@ -5,6 +5,14 @@
     non_upper_case_globals
 )]
 
-use libc::FILE;
+mod bindings {
+    use libc::FILE;
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+pub use crate::bindings::{fy_event__bindgen_ty_1 as fy_event_data, *};
+
+// Exclude the following types from being exported out of the bindings module.
+#[allow(dead_code)]
+struct fy_event__bindgen_ty_1;
