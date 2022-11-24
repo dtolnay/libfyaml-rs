5b6bf2269cda81165c648a57d44a3cbd3ba97ff7
David Tolnay <dtolnay@gmail.com>
2022-11-24 14:47:13 -0800

    Run bindgen on libfyaml.h from build.rs

--- libfyaml-sys/build.rs
+++ libfyaml-sys/build.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use std::env;
+use std::path::Path;
+
+fn main() {
+    let header = "libfyaml/include/libfyaml.h";
+    println!("cargo:rerun-if-changed={}", header);
+
+    let bindings = bindgen::Builder::default()
+        .header(header)
+        .generate()
+        .unwrap();
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_file = Path::new(&out_dir).join("bindings.rs");
+    bindings.write_to_file(out_file).unwrap();
+}
---
+++ libfyaml-sys/src/lib.rs
@@ -0,0 +1,8 @@
+#![allow(
+    improper_ctypes,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals
+)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
