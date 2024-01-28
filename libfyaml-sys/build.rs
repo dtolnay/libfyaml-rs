6992680093f8b6f8e9cae45321e5e6221f7351cc
Valentin Lab <valentin.lab@kalysto.org>
2024-01-28 13:28:55 +0100

    Compute version of libfyaml dynamically (fixes #3)
    We can enable back ``fy_library_version``.

--- libfyaml-sys/build.rs
+++ libfyaml-sys/build.rs
@@ -1,13 +1,58 @@
 #![allow(clippy::let_underscore_untyped, clippy::uninlined_format_args)]
 
+use git2::{DescribeFormatOptions, DescribeOptions, Repository};
 use regex::Regex;
+use semver::Version;
 use std::env;
+use std::error::Error;
 use std::ffi::OsStr;
 use std::fs::{DirEntry, File};
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 use std::process::{self, Command};
 
+fn generate_new_version(last_version: &str, commit_sha: &str) -> Result<String, String> {
+    // sanitize version string (add a patch number if missing)
+    let mut version = last_version.to_string();
+    let parts = last_version.split('.').collect::<Vec<&str>>();
+    // add ".0" one or 2 times
+    for _ in 0..(3 - parts.len()) {
+        version.push_str(".0");
+    }
+
+    let mut version = Version::parse(&version)
+        .map_err(|e| format!("Failed to parse {:?}: {}", last_version, e.to_string()))?;
+
+    version.patch += 1;
+
+    version.pre = semver::Prerelease::new("alpha")
+        .map_err(|e| format!("Failed to add pre-release: {}", e))?;
+    version.build = semver::BuildMetadata::new(commit_sha)
+        .map_err(|e| format!("Failed to add build-metadata: {}", e))?;
+
+    Ok(version.to_string())
+}
+
+fn latest_git_tag(repo_path: &str) -> Result<(String, Option<String>), Box<dyn Error>> {
+    let repo = Repository::open(repo_path)?;
+
+    let mut describe_options = DescribeOptions::new();
+    describe_options.describe_tags();
+
+    let mut format_options = DescribeFormatOptions::new();
+    format_options.abbreviated_size(7); // Full tag name
+
+    let describe = repo.describe(&describe_options)?;
+    let result = describe.format(Some(&format_options))?;
+    let result = result.trim_start_matches('v');
+    let result: Vec<&str> = result.split('-').collect();
+    // if there's more than one part, it's a post-release
+    if result.len() > 1 {
+        Ok((result[0].to_string(), Some(result[2].to_string())))
+    } else {
+        Ok((result[0].to_string(), None))
+    }
+}
 fn function_names(header_path: &str) -> impl Iterator<Item = String> {
     let file = File::open(header_path).expect("Unable to open file");
     let reader = BufReader::new(file);
@@ -42,8 +87,7 @@ fn main() {
         .header(header)
         .allowlist_recursively(false)
         .allowlist_function("fy_.*")
-        .allowlist_type("fy_.*")
-        .blocklist_function("fy_library_version");
+        .allowlist_type("fy_.*");
 
     // Variadic functions that use `va_list`.
     // Blocked on https://github.com/rust-lang/rust/issues/44930.
@@ -75,7 +119,14 @@ fn main() {
     build.flag_if_supported("-Wno-type-limits");
     build.flag_if_supported("-Wno-unused-but-set-parameter");
     build.flag_if_supported("-Wno-unused-parameter");
-    build.define("VERSION", "NULL");
+
+    let (version, commit) = latest_git_tag("libfyaml").unwrap();
+    if let Some(commit) = commit {
+        let new_version = generate_new_version(&version, &commit).unwrap();
+        build.define("VERSION", format!("{:?}", new_version).as_str());
+    } else {
+        build.define("VERSION", format!("{:?}", version).as_str());
+    }
     build.define("__STDC_WANT_LIB_EXT2__", "1");
     build.compile("libfyaml");
 }
